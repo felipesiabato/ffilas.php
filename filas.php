7ddddef0fd7c9ce75d9f483e2da70c333db58d0d
felipesiabato <siabatojuanfelipe77@gmail.com>
2025-05-28 08:16:39 -0500

    Add files via upload

---
+++ filas.php
@@ -0,0 +1,24 @@
+<?php
+
+
+
+$pila = array (); 
+
+array_push ($pila, "Manguita");
+array_push ($pila, "Laura");
+array_push ($pila, "Diego");
+array_push ($pila, "Felipe");
+array_push ($pila, "Juan");
+array_push ($pila, "Javier");
+array_push ($pila, "Andre");
+array_push ($pila, "Paula");
+array_push ($pila, "Karol");
+
+$ELEMENTO = array_pop($pila);
+
+echo "el ultimo elemento ingresado a la pila es: ". $ELEMENTO;
+
+
+
+
+?>
